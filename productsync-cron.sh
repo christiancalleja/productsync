635e434b4f984c8e3436f1904062d09a0749365f
CALLEJA-LAPTOP\Christian & Miriam <christiancalleja1@gmail.com>
2022-02-07 22:57:39 +0100

    cron logic sample finalized

--- productsync-cron.sh
+++ productsync-cron.sh
@@ -1,5 +1,9 @@
 # Bash ready for cronjob
 
 # Copy files from dropbox directory
-cp -a /root/Dropbox/CATALOGUE/. /var/www/trademargin/wp-content/uploads/catalogue/
+cp -a /root/Dropbox/CATALOGUE/. /var/www/[YOUR-SITE-NAME]/wp-content/uploads/catalogue/
+
+
+# Curl the magic at 4:05am every day 
+5 4 * * *  /usr/bin/curl --silent http://[YOUR-SITE-URL]/wp-json/productsync/v1/syncnow &>/dev/null
 
